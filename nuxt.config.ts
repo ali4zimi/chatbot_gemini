e929de379a7164ea8491f8da62d5d4ab45c75e67
Ali Azimi <azimi.ali@tum.com>
2025-04-16 13:52:09 +0200

    added tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  plugins: ['~/plugins/firebase.client.ts'],
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 })
